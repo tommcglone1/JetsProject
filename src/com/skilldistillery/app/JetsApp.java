ac799c6ca098fcf6065f61da3a5e0efb3cc394c3
Tom <tom.mcglone1@yahoo.com>
2023-01-27 16:18:14 -0500

    initial commit

---
+++ src/com/skilldistillery/app/JetsApp.java
@@ -0,0 +1,14 @@
+package com.skilldistillery.app;
+
+import com.skilldistillery.entities.AirField;
+
+public class JetsApp {
+	
+	private AirField airF;
+	
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ src/com/skilldistillery/entities/AirField.java
@@ -0,0 +1,7 @@
+package com.skilldistillery.entities;
+
+import java.util.List;
+
+public class AirField {
+	private List<Jet> jets;
+}
